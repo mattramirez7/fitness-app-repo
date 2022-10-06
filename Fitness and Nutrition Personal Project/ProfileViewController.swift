9f83116f5fefd7d3c03694c610d49838dbc5360c
mattramirez7 <77175367+mattramirez7@users.noreply.github.com>
2022-10-05 23:18:00 -0700

    Initial Commit

---
+++ Fitness and Nutrition Personal Project/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Fitness and Nutrition Personal Project
+//
+//  Created by Matthew Ramirez on 10/5/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fitness and Nutrition Personal Project/TitleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Fitness and Nutrition Personal Project
+//
+//  Created by Matthew Ramirez on 10/5/22.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
